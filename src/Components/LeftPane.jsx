e1932e8bfdf0167c9edcaa908fd4c3493793d0c7
tushar-droid <63186251+tushar-droid@users.noreply.github.com>
2024-10-20 21:46:14 -0700

    Added preview pages

---
+++ src/Components/LeftPane.jsx
@@ -0,0 +1,9 @@
+
+import '../styles/LeftPane.css'
+export default function LeftPane () {
+    return (
+        <div className="left-pane">
+            <h1>This is the left pane</h1>
+        </div>
+    )
+}
---
+++ src/Components/RightPane.jsx
@@ -0,0 +1,14 @@
+import '../styles/RightPane.css'
+
+export default function RightPane () {
+    return (
+        <div className="right-pane">
+            <div className='page-container'>
+                <div className='preview'>
+                </div>
+                <div className='preview page2' >
+                </div>
+            </div>
+        </div>
+    )
+}
